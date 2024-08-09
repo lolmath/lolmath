05a21d416c8bb72828be95353404f752b0a7b63e
Mees van Dongen <meesvandongen@gmail.com>
2024-08-09 21:55:35 +0200

    tabs

--- packages/ui/src/components/tabs.tsx
+++ packages/ui/src/components/tabs.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext, useContext } from "react";
+import { cva, cx } from "cva";
 import {
 	Tab as AriaTab,
 	TabList as AriaTabList,
@@ -9,34 +9,13 @@ import {
 	type TabListProps,
 	type TabPanelProps,
 	type TabProps,
-	type TabsProps,
 } from "react-aria-components";
-import { useCssId } from "../utilities/css-id.js";
 import { resolveClassName } from "../utilities/resolve-class-name.js";
-import { tv } from "../utilities/tv.js";
-import { startViewTransition } from "../utilities/view-transition.js";
+import classes from "./tabs.module.css";
 
 export type { Key } from "react-aria-components";
 
-export function Tabs({ onSelectionChange, ...rest }: TabsProps) {
-	const id = useCssId();
-	return (
-		<TabsContext.Provider value={{ id }}>
-			<AriaTabs
-				{...rest}
-				onSelectionChange={(key) => {
-					startViewTransition(() => {
-						onSelectionChange?.(key);
-					});
-				}}
-			/>
-		</TabsContext.Provider>
-	);
-}
-
-const tabList = tv({
-	base: "-ml-4 flex gap-0",
-});
+export const Tabs = AriaTabs;
 
 export function TabList<T extends object>({
 	className,
@@ -46,37 +25,34 @@ export function TabList<T extends object>({
 		<AriaTabList<T>
 			{...rest}
 			className={(values) =>
-				tabList({
-					className: resolveClassName(className, values),
-				})
+				cx(classes.list, resolveClassName(className, values))
 			}
 		/>
 	);
 }
 
-const tab = tv({
-	base: [
-		"font-beaufort text-lol-gold-300 relative cursor-pointer select-none px-4 py-1 text-xs font-medium uppercase tracking-widest",
-		"focus-visible:outline-lol-gold-100 focus:outline-none focus-visible:outline-1 focus-visible:outline-offset-4",
-	],
+const tab = cva({
+	base: classes.tab,
 	variants: {
 		isSelected: {
-			true: "text-lol-gold-100",
+			true: classes.selected,
 		},
 		isHovered: {
-			true: "text-lol-gold-100",
+			true: classes.hovered,
 		},
 		isPressed: {
-			true: "text-lol-gold-500",
+			true: classes.pressed,
 		},
 		isDisabled: {
-			true: "text-lol-grey-150 cursor-default",
+			true: classes.disabled,
+		},
+		isFocusVisible: {
+			true: classes.focusVisible,
 		},
 	},
 });
 
 export function Tab({ children, className, ...rest }: TabProps) {
-	const { id } = useTabsContext();
 	return (
 		<AriaTab
 			{...rest}
@@ -90,16 +66,7 @@ export function Tab({ children, className, ...rest }: TabProps) {
 			{(values) => (
 				<>
 					{typeof children === "function" ? children(values) : children}
-					{values.isSelected && (
-						<div
-							style={{
-								viewTransitionName: `tab-indicator-${id}`,
-							}}
-							className={
-								"via-lol-gold-200 absolute bottom-0 left-0 h-px w-full bg-gradient-to-r from-transparent to-transparent"
-							}
-						/>
-					)}
+					{values.isSelected && <div className={classes.indicator} />}
 				</>
 			)}
 		</AriaTab>
@@ -108,19 +75,4 @@ export function Tab({ children, className, ...rest }: TabProps) {
 
 export function TabPanel({ ...rest }: TabPanelProps) {
 	return <AriaTabPanel {...rest} />;
-}
-
-const TabsContext = createContext<
-	| {
-			id: string;
-	  }
-	| undefined
->(undefined);
-
-function useTabsContext() {
-	const context = useContext(TabsContext);
-	if (context === undefined) {
-		throw new Error("useTabsContext must be used within a Tabs");
-	}
-	return context;
 }
