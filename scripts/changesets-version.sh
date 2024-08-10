366fe89bafca6ce13f150148b6b1b96d75f7b5a7
Mees van Dongen <meesvandongen@gmail.com>
2024-08-11 01:27:24 +0200

    pipeline

--- scripts/changesets-version.sh
+++ scripts/changesets-version.sh
@@ -1,5 +1,7 @@
 if [ "$CI_COMMIT_BRANCH" = "main" ]; then
-pnpm run changeset version
+	pnpm run changeset version
 else
-pnpm run changeset version --snapshot
+	pnpm run changeset version --snapshot
+	pnpm run changeset publish --tag canary --no-git-tag
+	git reset --hard HEAD~1
 fi
