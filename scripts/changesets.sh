81fdc176c61670b6e7f3294ca9a87a48cc33d8d8
Mees van Dongen <meesvandongen@gmail.com>
2024-08-11 11:01:55 +0200

    double brackets

--- scripts/changesets.sh
+++ scripts/changesets.sh
@@ -1,7 +1,7 @@
-if [ "$CI_COMMIT_BRANCH" = "main" ]; then
-  npx changesets-gitlab
+if [[ "$CI_COMMIT_BRANCH" = "main" ]]; then
+	npx changesets-gitlab
 elif [[ "$CI_COMMIT_BRANCH" != "main" && "$CI_COMMIT_BRANCH" != changeset-release* ]]; then
-  pnpm run changeset version --snapshot
-  echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
-  pnpm run changeset publish --tag canary --no-git-tag
+	pnpm run changeset version --snapshot
+	echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
+	pnpm run changeset publish --tag canary --no-git-tag
 fi
