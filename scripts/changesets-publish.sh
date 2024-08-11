07757e053d84126e0e06bd8b3da048756b748e56
Mees van Dongen <meesvandongen@gmail.com>
2024-08-11 10:54:36 +0200

    ci

--- scripts/changesets-publish.sh
+++
@@ -1,5 +0,0 @@
-if [ "$CI_COMMIT_BRANCH" = "main" ]; then
-	pnpm run changeset publish
-else
-	pnpm run changeset publish --tag canary --no-git-tag
-fi
--- scripts/changesets-version.sh
+++
@@ -1,8 +0,0 @@
-if [ "$CI_COMMIT_BRANCH" = "main" ]; then
-	pnpm run changeset version
-else
-	pnpm run changeset version --snapshot
-	echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
-	pnpm run changeset publish --tag canary --no-git-tag
-	git reset --hard HEAD~1
-fi
