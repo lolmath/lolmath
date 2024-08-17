54d1aeaf21375cd7f31fb371684dfddfd2b19ebb
Mees van Dongen <meesvandongen@gmail.com>
2024-08-17 17:14:20 +0200

    fix text

--- packages/ui/src/components/typography/text.tsx
+++ packages/ui/src/components/typography/text.tsx
@@ -1,9 +1,9 @@
-import { cva } from "cva";
 import { createElement } from "react";
 import {
 	Label as AriaLabel,
 	type LabelProps as AriaLabelProps,
 } from "react-aria-components";
+import { cva } from "cva";
 import classes from "./text.module.css";
 
 export type TextColor = "grey100" | "grey150" | "gold100";
@@ -29,7 +29,7 @@ const text = cva({
 		},
 		preset: {
 			sm: classes.sm,
-			medium: classes.medium,
+			base: classes.base,
 			md: classes.md,
 			lg: classes.lg,
 			label: classes.label,
