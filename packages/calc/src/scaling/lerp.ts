e68b5f6f2f869ab37c9545100928b730655ad9f3
Redymix <arhheling@gmail.com>
2025-05-31 21:59:30 -0500

    Add lerp3, lerp5, lerp18

--- packages/calc/src/scaling/lerp.ts
+++ packages/calc/src/scaling/lerp.ts
@@ -1,3 +1,4 @@
+import { linear3, linear5, linear18 } from "./linear";
 /**
  * Interpolates a value between a minimum and maximum based on a given ratio.
  *
@@ -20,4 +21,55 @@
  */
 export function lerp(min: number, max: number, ratio: number): number {
 	return (1 - ratio) * min + ratio * max;
+}
+
+/**
+ * Interpolates a value between min and max based on a 3-level scaling.
+ *
+ * @param min - The minimum value.
+ * @param max - The maximum value.
+ * @param level - The current level (1 to 3).
+ * @returns The interpolated value.
+ *
+ * @example
+ * ```ts
+ * const damage = lerp3(80, 180, 2); // 130
+ * ```
+ */
+export function lerp3(min: number, max: number, level: number): number {
+	return lerp(min, max, linear3(level));
+}
+
+/**
+ * Interpolates a value between min and max based on a 5-level scaling.
+ *
+ * @param min - The minimum value.
+ * @param max - The maximum value.
+ * @param level - The current level (1 to 5).
+ * @returns The interpolated value.
+ *
+ * @example
+ * ```ts
+ * const damage = lerp5(80, 260, 3); // 170
+ * ```
+ */
+export function lerp5(min: number, max: number, level: number): number {
+	return lerp(min, max, linear5(level));
+}
+
+/**
+ * Interpolates a value between min and max based on an 18-level scaling.
+ *
+ * @param min - The minimum value.
+ * @param max - The maximum value.
+ * @param level - The current level (1 to 18).
+ * @returns The interpolated value.
+ *
+ * @example
+ * ```ts
+ * const damage = lerp18(80, 260, 10); // 170
+ * ```
+ */
+export function lerp18(min: number, max: number, level: number): number {
+	return lerp(min, max, linear18(level));
 }
