c0744b97ffd49aa3fe228358c5f3bade4f67db68
Mees van Dongen <meesvandongen@gmail.com>
2025-08-31 11:45:45 +0200

    ..

--- packages/calc/typedoc.mjs
+++ packages/calc/typedoc.mjs
@@ -1,59 +1,11 @@
-import { writeFileSync } from "node:fs";
-import { MarkdownPageEvent } from "typedoc-plugin-markdown";
+import lolmathConfig from "@lolmath/typedoc";
+import pluginFumaDocsSidebar from "@lolmath/typedoc/fumadocs";
 
 /** @type {import("typedoc").TypeDocOptions} */
 const config = {
+	...lolmathConfig,
 	entryPoints: ["./src/*/index.ts"],
-	out: "./docs",
-	plugin: [
-		"typedoc-plugin-markdown",
-		"typedoc-plugin-frontmatter",
-		// FrontMatter
-		(app) => {
-			app.renderer.on(
-				MarkdownPageEvent.BEGIN,
-				/** @param {import('typedoc-plugin-markdown').MarkdownPageEvent} page */
-				(page) => {
-					if (page.model?.name) {
-						page.frontmatter.title = page.model.name;
-					}
-				},
-			);
-		},
-		// Sidebar
-		(app) => {
-			app.renderer.postRenderAsyncJobs.push(async (_renderer) => {
-				const outDir = app.options.getValue("out");
-				writeFileSync(
-					`${outDir}/meta.json`,
-					JSON.stringify({
-						title: "@lolmath/calc",
-						root: true,
-					}),
-				);
-			});
-		},
-	],
-	mergeReadme: true,
-	hidePageHeader: true,
-	hideBreadcrumbs: true,
-	hidePageTitle: true,
-	hideGroupHeadings: true,
-	useCodeBlocks: true,
-	disableSources: false,
-	parametersFormat: "table",
-	typeAliasPropertiesFormat: "table",
-	enumMembersFormat: "table",
-	propertyMembersFormat: "table",
-	typeDeclarationFormat: "table",
-	pageTitleTemplates: {
-		member: "`{rawName}`",
-	},
-	entryFileName: "index",
-	defaultCategory: "Other",
-	router: "module",
-	gitRevision: "main",
-	publicPath: "./",
+	plugin: [...lolmathConfig.plugin, pluginFumaDocsSidebar("@lolmath/calc")],
 };
 
 export default config;
